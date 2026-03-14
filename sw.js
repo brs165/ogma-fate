1b064015f519dd584c77ea855b5aceba5c6916f3
brs165 <brs165@users.noreply.github.com>
2026-03-14 19:02:28 -0400

    fate-generator-2026.03.22

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Strategy: cache-first for all static assets, network-first for CDN scripts.
 // Safari fix: redirected responses are cloned as non-redirected before caching.
 
-var CACHE_NAME = 'fate-generator-2026.03.18';
+var CACHE_NAME = 'fate-generator-2026.03.22';
 
 var APP_SHELL = [
   './index.html',
