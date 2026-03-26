ca99dcd9d07c66758527a2b4ef67979aa4734f59
brs165 <brs165@users.noreply.github.com>
2026-03-26 06:39:16 +0000

    fix: move ssr=false to +layout.js, not +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-export const ssr = falseS;
+export const ssr = false;
 export const prerender = false;
   // Fonts loaded via svelte:head; theme.css loaded via app.html link tag
 </script>
